a5e8aec80215f3736aef42a01399abf3f0a5f9cb
xamayca <christopher.hariouat@gmail.com>
2024-02-02 10:58:51 +0100

    add nav + fix README.MD + creating 1 route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,5 +17,13 @@ class HomeController extends AbstractController
         ]);
     }
 
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
 
 }
